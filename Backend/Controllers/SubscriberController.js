e0d6757e02b8997d58b75b2e4b98d41aa2795e0a
Jonathan Donkoh <jonathandonkoh7@gmail.com>
2026-08-12 01:40:01 +0000

    Fix case-sensitive import for SubscriberModule

--- Backend/Controllers/SubscriberController.js
+++ Backend/Controllers/SubscriberController.js
@@ -1,5 +1,5 @@
 const { Resend } = require("resend");
-const SubscriberModel = require("../Models/SubscriberModule");
+const SubscriberModel = require("../Models/subscribermodule");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
